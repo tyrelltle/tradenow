8ae950a38bf37569e5ada13dcc6b7a86eb78b9c7
tsa45 <tsa45@ubuntu.ubuntu-domain>
2014-04-19 10:42:06 -0700

    add basic hibernate test env

--- LeoTrade/src/test/java/com/tianshao/cuige/test.java
+++ LeoTrade/src/test/java/com/tianshao/cuige/test.java
@@ -1,59 +1,27 @@
 package com.tianshao.cuige;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;  
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;  
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*; 
-//import static org.springframework.test.web.servlet.result.ModelResultMatchers.*;
+
+import com.tianshao.cuige.models.Product;
+import com.tianshao.cuige.services.ProductService;
 
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = {"classpath:hibernate.cfg.xml", "classpath:servlet-context.xml"})
-//@WebAppConfiguration
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:servlet-context.xml"})
 public class test {
-//	@Autowired
-//	private WebApplicationContext wac;
-//	
-//	private MockMvc mockMvc;
-//	
-//	@Before
-//	public void setup() {
-//        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build(); 
-//
-//	}
-//	@Test
-//	public void addTest() throws Exception {
-//		   ResultActions a=mockMvc.perform(post("/event").contentType(MediaType.APPLICATION_JSON)
-//													   .accept(MediaType.APPLICATION_JSON)
-//													   .content("{\"id\":\"0\",\"title\":\"newbe\",\"loc\":\"newbe\",\"detail\":\"newbe\"}"));;
-//			
-//		   a=mockMvc.perform(post("/event").contentType(MediaType.APPLICATION_JSON)
-//															   .accept(MediaType.APPLICATION_JSON)
-//															   .content("{\"id\":\"1\",\"title\":\"newbe2\",\"loc\":\"newbe2\",\"detail\":\"newbe2\"}"));;
-//		   a=mockMvc.perform(get("/event"));
-//		   //TODO At this point, no get(id) api is defined yet, so 
-//	/*	    try {
-//		    
-//		    	a.andExpect(jsonPath("$[0].title").value("asd"))
-//		    	 .andExpect(jsonPath("$[0].loc").value("asd"))
-//		    	 .andExpect(jsonPath("$[0].detail").value("asd"));
-//			} catch (Exception e) {
-//				
-//				e.printStackTrace();
-//			}*/
-//		}
+	@Autowired
+	ProductService p;
+	
+	@Test
+	public void hellworld(){
+		List<Product> l=p.getProducts();
+		l.clear();
+	}
 }
