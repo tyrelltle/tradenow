f1b96014db871ac9a50b076378b2cd570ea96df1
tyrelltle <tyrelltle@gmail.com>
2014-11-17 23:50:20 -0800

    update TEMPORARY delete user script

--- LeoTrade/sql/batch/deleteuser.bat
+++ LeoTrade/sql/batch/deleteuser.bat
@@ -1,9 +1,23 @@
 use leotrade;
-delete from userconnection where userid=75;
-delete from favorite where userid = 75;
-delete from product where ownerid=75;
-delete from notification where userid=75;
-delete from regconfirm where userid=75;
-delete from user_roles where userid=75;
-delete from user where userid=75;
+delete from userconnection where userid=43;
+delete from favorite where userid = 43;
+delete from favorite where prod_id in (select prod_id from product where ownerid=43);
+
+delete from message where trade_id in (select trade_id from trade where p
+rod1_id in (select prod_id from product where ownerid=43));
+
+delete from message where trade_id in (select trade_id from trade where p
+rod2_id in (select prod_id from product where ownerid=43));
+
+delete from trade where prod1_id in (select prod_id from product where ow
+nerid=43);
+
+delete from trade where prod2_id in (select prod_id from product where ow
+nerid=43);
+
+delete from product where ownerid=43;
+delete from notification where userid=43;
+delete from regconfirm where userid=43;
+delete from user_roles where userid=43;
+delete from user where userid=43;
 exit
